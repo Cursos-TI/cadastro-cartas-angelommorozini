bf30d36feae8f68deb17175487bb7c5909a61036
Angelo Morozini <angelomorozini_809@hotmail.com>
2026-03-05 03:12:58 +0000

    Completei o desafio nível novato.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,64 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado_1[20];
+    char estado_2[20];
+    char carta_1;
+    char carta_2;
+    char cidade_1[20];
+    char cidade_2[20];
+    int populacao_1;
+    int populacao_2;
+    float area_1;
+    float area_2;
+    float pib_1;
+    float pib_2;
+    int pontos_1;
+    int pontos_2;
 
-  // Área para entrada de dados
+    printf("\n--- CARTA 1 ---\n");
+    
+    printf("Digite seu Estado: ");
+    scanf("%s", estado_1);
 
-  // Área para exibição dos dados da cidade
+    printf("Digite o código da carta: ");
+    scanf(" %c",&carta_1);
 
-return 0;
+    printf("Digite o nome da cidade: ");
+    scanf("%s", cidade_1);
+
+    printf("Digite o número da população: ");
+    scanf("%d", &populacao_1);
+
+    printf("Digite a área: ");
+    scanf("%f", &area_1);
+
+    printf("Digite o valor do PIB: ");
+    scanf("%f", &pib_1);
+
+    printf("Digite números de pontos turisticos: ");
+    scanf("%d", &pontos_1);
+
+    printf("\n--- CARTA2 ---\n");
+
+    printf("Digite seu Estado: ");
+    scanf("%s", estado_2);
+
+    printf("Digite o código da carta: ");
+    scanf(" %c",&carta_2);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", cidade_2);
+
+    printf("Digite o número da população: ");
+    scanf("%d", &populacao_2);
+
+    printf("Digite a área: ");
+    scanf("%f", &area_2);
+
+    printf("Digite o valor do PIB: ");
+    scanf("%f", &pib_2);
+
+    printf("Digite números de pontos turisticos: ");
+    scanf("%d", &pontos_2);
 } 
